798b43f09f90e0bcb5c0abc1330063cfe3be29e9
hamza alahmad <hamza.a.alahmad@outlook.com>
2026-05-15 20:58:36 +0300

    add-migration

--- Blocko.Persistence/Migrations/20260515171306_cart--mig2.Designer.cs
+++ Blocko.Persistence/Migrations/20260515171306_cart--mig2.Designer.cs
@@ -4,6 +4,7 @@ using Blocko.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Blocko.Persistence.Migrations
 {
     [DbContext(typeof(BlockoDbContext))]
-    partial class BlockoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260515171306_cart--mig2")]
+    partial class cartmig2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -35,9 +38,6 @@ namespace Blocko.Persistence.Migrations
 
                     b.Property<int>("DisplayOrder")
                         .HasColumnType("int");
-
-                    b.Property<string>("ImageUrl")
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Name")
                         .IsRequired()
--- Blocko.Persistence/Migrations/20260515174657_cart--mig3.Designer.cs
+++ Blocko.Persistence/Migrations/20260515174657_cart--mig3.Designer.cs
@@ -4,6 +4,7 @@ using Blocko.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Blocko.Persistence.Migrations
 {
     [DbContext(typeof(BlockoDbContext))]
-    partial class BlockoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260515174657_cart--mig3")]
+    partial class cartmig3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
